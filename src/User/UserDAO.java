4c9dfae5a46c5f50c824809bfe3a808d56cac825
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-03 09:51:14 -0500

    Car Booking structure and file structure set up for user DAO class.

---
+++ src/User/UserDAO.java
@@ -0,0 +1,11 @@
+package User;
+
+/**
+ *      Data Access Object (DAO) class for managing User objects.
+ *      This implementation stores and manages users using arrays.
+ *
+ *      TODO: Implement user DAO functionality
+ */
+
+public class UserDAO {
+}
