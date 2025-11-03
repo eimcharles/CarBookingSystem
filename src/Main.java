f071efeaae1d35769bb62e660cdaaa28afb20cf6
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-03 09:02:05 -0500

    Testing User domain class methods

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        User testUser = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer");
+        User testUser1 = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer");
+        User testUser2 = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer");
+        User testUser3 = new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Larry", "Eimer");
 
-        System.out.println(testUser);
+        System.out.println(testUser1);
+        System.out.println(testUser1.equals(testUser2));
+        System.out.println(testUser1.equals(testUser3));
 
     }
 }
