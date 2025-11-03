9622461e05b290d5a90b958e2d56a4c49c3d46c4
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-03 08:51:00 -0500

    Defining User Domain Class and creating a test user object to ensure consistency.

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,17 @@
 *      TODO: Implement Driver Class
 * */
 
+import User.User;
+
+import java.util.UUID;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        User testUser = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer");
+
+        System.out.println(testUser);
 
     }
 }
