480541b3851f64269e33e0e2cb6a4ec10b9ac5d2
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-03 10:07:33 -0500

    Defining User DAO class and creating test userDAO objects to ensure consistency.

--- src/Main.java
+++ src/Main.java
@@ -6,35 +6,15 @@
 *      TODO: Implement Driver Class
 * */
 
-import Booking.Booking;
-import Car.Car;
+import User.UserDAO;
 
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.UUID;
-
-import User.User;
-import Car.Brand;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Unique userId and bookingId for a new booking not cancelled
-        User testUser1 = new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer");
-        Car testCar1 = new Car("1234", new BigDecimal("89.00"), Brand.BMW, false);
-        Booking testBooking1 = new Booking(UUID.fromString("576590ff-57a1-4df3-8430-79980eb42343"), testUser1, testCar1, LocalDateTime.now());
-        System.out.println(testBooking1);
-
-        // Unique userId and bookingId for a new booking not cancelled
-        User testUser2 = new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Larry", "Eimer");
-        Car testCar2 = new Car("1235", new BigDecimal("109.00"), Brand.VOLKSWAGEN, false);
-        Booking testBooking2 = new Booking(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), testUser2, testCar2, LocalDateTime.now());
-        System.out.println(testBooking2);
-
-        System.out.println(testBooking1.equals(testBooking2));
-        System.out.println(testBooking2.getUserBookingID().equals(testBooking1.getUserBookingID()));
-        System.out.println(testBooking1.getBookingTime());
+        System.out.println(Arrays.toString(UserDAO.getUserDAO()));
 
     }
 }
--- src/User/UserDAO.java
+++ src/User/UserDAO.java
@@ -1,11 +1,27 @@
 package User;
 
+import java.util.UUID;
+
 /**
  *      Data Access Object (DAO) class for managing User objects.
  *      This implementation stores and manages users using arrays.
- *
- *      TODO: Implement user DAO functionality
  */
 
 public class UserDAO {
+
+    private static final User[] userDAO;
+
+    static {
+
+        userDAO = new User[]{
+
+            new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer"),
+            new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Jerry", "Leblond"),
+
+        };
+    }
+
+    public static User[] getUserDAO() {
+        return userDAO;
+    }
 }
