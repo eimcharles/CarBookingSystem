f6e09b4c6c10bd9ec65500c7225fc58aac6f16fb
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-18 12:11:23 -0500

    Testing refactored changes to ensure no breaks in code

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@
 *      TODO: Implement Driver Class
 * */
 
+import Booking.Booking;
+import Booking.BookingDAO;
 import Car.Car;
 import Car.CarService;
 import Car.CarDAO;
@@ -13,6 +15,7 @@ import User.User;
 import User.UserService;
 import User.UserDAO;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -23,7 +26,7 @@ public class Main {
         // Get the user from the user DAO
         UserDAO userDAO = new UserDAO();
         UserService userService = new UserService(userDAO);
-        User testUser = userService.getUsersByIdFromDAO(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"));
+        User testUser = userService.getUsersById(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"));
 
         // Get the car from the car DAO
         CarDAO carDAO = new CarDAO();
@@ -33,21 +36,21 @@ public class Main {
         System.out.println(Arrays.toString(carService.getGasolineCars()));
 
 
-//        // Create a new booking Object with the test user and car
-//        Booking testBooking = new Booking(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), testUser, testCar, LocalDateTime.now());
-//        Booking testBooking2 = new Booking(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), testUser, testCar, LocalDateTime.now());
+        // Create a new booking Object with the test user and car
+        Booking testBooking = new Booking(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), testUser, testCar, LocalDateTime.now());
+        Booking testBooking2 = new Booking(UUID.fromString("9d818235-ce3b-40e8-b74a-3674985c6bcd"), testUser, testCar, LocalDateTime.now());
 
 
-//        BookingDAO bookingDAO = new BookingDAO();
-//
-//        // Add the booking to the bookingDAO
-//        bookingDAO.addBooking(testBooking);
-//        bookingDAO.addBooking(testBooking);
-//        bookingDAO.addBooking(testBooking);
-//
-//
-//        // Display the whole list of bookings in the booking DAO
-//        System.out.println(Arrays.toString(BookingDAO.getBookingsDao()));
+        BookingDAO bookingDAO = new BookingDAO();
+
+        // Add the booking to the bookingDAO
+        bookingDAO.addBooking(testBooking);
+        bookingDAO.addBooking(testBooking);
+        bookingDAO.addBooking(testBooking);
+
+
+        // Display the whole list of bookings in the booking DAO
+        System.out.println(Arrays.toString(BookingDAO.getBookingsDao()));
 
     }
 }
