820881af23a54d7e0187720b7c2a8c20292dccb3
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-18 14:48:06 -0500

    Implementing the addBooking() in BookingDAO with exception handling.
    Implementing getBookingDao() by using defensive copying for data integrity.
    
    Adding a static constant MAX_CAPACITY for the bookingsDao array.
    
    Adding nextAvailableIndex to track the next available slot in bookingsDao array.

--- src/Booking/BookingDAO.java
+++ src/Booking/BookingDAO.java
@@ -1,5 +1,7 @@
 package Booking;
 
+import java.util.Arrays;
+
 /**
  *      Data Access Object (DAO) class for managing Car Booking objects.
  *      This implementation stores and manages Car Bookings using arrays.
@@ -11,14 +13,35 @@ public class BookingDAO {
 
     private static final Booking[] bookingsDao;
 
+    // Tracks the index of the next available slot in bookingsDao
+    private static int nextAvailableIndex = 0;
+
+    // Defines the fixed, maximum size of the array storage.
+    private static final int MAX_CAPACITY = 10;
+
     static {
 
-        // Maximum available bookings
-        bookingsDao = new Booking[10];
+        // Storage for available bookings
+        bookingsDao = new Booking[MAX_CAPACITY];
 
     }
 
-    public static Booking[] getBookingsDao() {
-        return bookingsDao;
+    public static void addBooking(Booking carBooking) {
+
+        // Checks if index of next available slot has reached length of array
+        if (nextAvailableIndex >= bookingsDao.length) {
+            throw new IllegalStateException(String.format("No more bookings available, total slots available %d", bookingsDao.length));
+        }
+
+        // Adds the carBooking into the slot that nextAvailableIndex points to
+        bookingsDao[nextAvailableIndex] = carBooking;
+
+        // Moves the index to point to the following slot
+        nextAvailableIndex++;
     }
+
+    public static Booking[] getBookingDao() {
+        return Arrays.copyOf(bookingsDao, bookingsDao.length);
+    }
+
 }
