f0f10d02a2bdd664522843b096d9a3a13ce48838
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-19 13:58:45 -0500

    Implementing core car booking creation and availability checks: addCarBooking(), isCarCurrentlyBooked() and getAllBookings()

--- src/Booking/BookingService.java
+++ src/Booking/BookingService.java
@@ -1,5 +1,12 @@
 package Booking;
 
+import Car.Car;
+import Car.CarService;
+import User.User;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 /**
  *      Service class for managing Booking objects.
  *      Contains business logic related to bookings.
@@ -8,4 +15,109 @@ package Booking;
  */
 
 public class BookingService {
+
+    private final BookingDAO bookingDAO;
+    private final CarService carService;
+
+    public BookingService(BookingDAO bookingDAO, CarService carService) {
+        this.bookingDAO = bookingDAO;
+        this.carService = carService;
+    }
+
+    public UUID addCarBooking(User user, String registrationNumber) {
+
+        // Finds car based on registration number
+        Car carToBook = this.carService.getCarByRegistrationNumber(registrationNumber);
+
+        // Check Availability - is it already booked ?
+        if (isCarCurrentlyBooked(registrationNumber)) {
+            throw new IllegalStateException(String.format("Car with registration number %s is currently booked.", registrationNumber));
+        }
+
+        // if car is not booked and available - create a new bookingId
+        UUID bookingId = UUID.randomUUID();
+
+        // Create and Save Booking in bookingDAO
+        this.bookingDAO.addBooking(
+                new Booking(
+                        bookingId,
+                        user,
+                        carToBook,
+                        LocalDateTime.now()
+                )
+        );
+
+        // return the booking id
+        return bookingId;
+    }
+
+    private boolean isCarCurrentlyBooked(String registrationNumber) {
+
+        // Gets all current bookings
+        Booking[] allBookings = getAllBookings();
+
+        // Go through current bookings
+        for (Booking booking : allBookings) {
+
+            // Only executes for active (uncancelled) bookings.
+            if (!booking.isBookingCancelled()) {
+
+                // Check if the car's registration matches the requested one
+                if (booking.getCar().getRegistrationNumber().equals(registrationNumber)) {
+
+                    // The booking is active AND the registration numbers match
+                    return true;
+                }
+            }
+        }
+
+        // did not find an active booking matching the requested registration number.
+        return false;
+    }
+
+    public Booking[] getAllBookings() {
+
+        int allActiveBookingsCount = 0;
+
+        // Booking returned from DAO layer
+        Booking[] bookings = this.bookingDAO.getBookingDao();
+
+        // If bookings is null or empty, return empty array
+        if (bookings == null || bookings.length == 0){
+            return new Booking[0];
+        }
+
+        // Count the active bookings
+        for (Booking booking : bookings) {
+            if (booking != null){
+                allActiveBookingsCount++;
+            }
+        }
+
+        // If no active bookings are found
+        if (allActiveBookingsCount == 0) {
+            return new Booking[0];
+        }
+
+        // Create a new array with the allActiveBookingsCount for size
+        Booking[] filteredBookings = new Booking[allActiveBookingsCount];
+
+        // Index for the new array, avoids null gaps,
+        int index = 0;
+
+        // Look through all the bookings
+        for (int i = 0; i < bookings.length; i++) {
+
+            // Use the activeBooking at the current index of the source array
+            Booking activeBooking = bookings[i];
+
+            if (activeBooking != null){
+                filteredBookings[index++] = activeBooking;
+            }
+
+        }
+
+        return filteredBookings;
+
+    }
 }
