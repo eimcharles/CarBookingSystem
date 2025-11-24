bf66f294755c464558e2a5b9b5a4a819a169b5e7
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-24 15:26:58 -0500

    Testing / Verify displayUserBookedCars()
     The method uses a do-while loop with try-catch to ensure the user provides a valid, non-empty UUID before querying the Booking Service for car reservations that are associated to inputted passed userID.

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -3,6 +3,8 @@ package Test;
 import Configuration.Configuration;
 import App.CarBookingCLI;
 
+import java.util.Scanner;
+
 /**
  *      Test.Test class for Car Booking System CLI
  * */
@@ -12,34 +14,34 @@ public class Test {
     public static void main(String[] args) {
 
         Configuration configuration = new Configuration();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Display all users feature");
-        CarBookingCLI.displayAllUsers(configuration.getUserService());
+//        System.out.println("Display all users feature");
+//        CarBookingCLI.displayAllUsers(configuration.getUserService());
+//        System.out.println();
+
+//        System.out.println("Display all available cars feature: ");
+//        CarBookingCLI.displayAllAvailableCars(configuration.getBookingService());
+//        System.out.println();
+
+//        System.out.println("Display all available electric cars feature: ");
+//        CarBookingCLI.displayAllAvailableElectricCars(configuration.getBookingService());
+//        System.out.println();
+
+//        System.out.println("Display all available gas cars feature: ");
+//        CarBookingCLI.displayAllAvailableGasCars(configuration.getBookingService());
+//        System.out.println();
+
+        CarBookingCLI.displayUserBookedCars(configuration.getUserService(), configuration.getBookingService(), scanner);
         System.out.println();
 
-        System.out.println("Display all available cars feature: ");
-        CarBookingCLI.displayAllAvailableCars(configuration.getBookingService());
-        System.out.println();
+//        System.out.println("Display all active bookings feature (No current bookings): ");
+//        CarBookingCLI.displayAllBooking(configuration.getBookingService());
+//        System.out.println();
 
-        System.out.println("Display all available electric cars feature: ");
-        CarBookingCLI.displayAllAvailableElectricCars(configuration.getBookingService());
-        System.out.println();
-
-        System.out.println("Display all available gas cars feature: ");
-        CarBookingCLI.displayAllAvailableGasCars(configuration.getBookingService());
-        System.out.println();
-
-        System.out.println("Display user booked cars gas cars feature TO DO : ");
-        CarBookingCLI.displayUserBookedCars(configuration.getUserService());
-        System.out.println();
-
-        System.out.println("Display all active bookings feature (No current bookings): ");
-        CarBookingCLI.displayAllBooking(configuration.getBookingService());
-        System.out.println();
-
-        System.out.println("Make a car booking feature TO DO : ");
-        CarBookingCLI.makeACarBooking(configuration.getBookingService());
-        System.out.println();
+//        System.out.println("Make a car booking feature TO DO : ");
+//        CarBookingCLI.makeACarBooking(configuration.getBookingService(), null, null);
+//        System.out.println();
 
     }
 }
