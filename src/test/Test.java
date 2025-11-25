cffd844564163ea08a0209da41311f29c955d7c8
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-25 15:48:12 -0500

    Testing displayBookingDetailsFormatted for comprehensive booking view

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,35 +1,38 @@
 package test;
 
+import booking.Booking;
+import car.Brand;
+import car.Car;
+import car.FuelType;
+import user.User;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static utility.CLIFormatUtility.*;
+
 /**
  *      Test.Test class for Car Booking System CLI
  * */
 
 public class Test {
 
-//    public static void main(String[] args) {
-//
-////        Configuration configuration = new Configuration();
-////        Scanner scanner = new Scanner(System.in);
-//
-////        // Done
-////        CarBookingCLI.displayAllUsers(configuration.getUserService());
-////
-////        // Done
-////        CarBookingCLI.displayAllAvailableCars(configuration.getBookingService());
-////
-////        // Done
-////        CarBookingCLI.displayAllAvailableElectricCars(configuration.getBookingService());
-////
-////        // Done
-////        CarBookingCLI.displayAllAvailableGasCars(configuration.getBookingService());
-////
-////        // Done
-////        CarBookingCLI.displayAllBooking(configuration.getBookingService());
-//
-////        // Done
-////        CarBookingCLI.displayUserBookedCars(configuration.getUserService(), configuration.getBookingService(), scanner);
-//
-////        CarBookingCLI.makeACarBooking(configuration.getUserService(), configuration.getBookingService(), scanner);
-//
-//    }
+    public static void main(String[] args) {
+
+//        Configuration configuration = new Configuration();
+//        Scanner scanner = new Scanner(System.in);
+
+        displayResultsMenu(TITLE_ALL_BOOKINGS);
+
+        displayBookingDetailsFormatted(
+                new Booking(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"),
+                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "Charles", "Eimer"),
+                        new Car("123_1", new BigDecimal("89.00"), Brand.BMW, FuelType.ELECTRIC),
+                        LocalDateTime.now()
+                ));
+
+//        CarBookingCLI.makeACarBooking(configuration.getUserService(), configuration.getBookingService(), scanner);
+
+    }
 }
