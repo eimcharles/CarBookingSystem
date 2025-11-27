8bdaf01867673a02e740048dd26041539ecf54ee
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-27 15:15:25 -0500

    Refactor(CLIFormatUtility): Cleaning up the CLIFormatUtility utility class.
    - Removing unnecessary methods
    - Renaming constants for better clarity
    - Renaming methods for better clarity
    - Adding comment to describe class role and functionality.
    - Changing menu items and titles for better clarity

--- src/utility/CLIFormatUtility.java
+++ src/utility/CLIFormatUtility.java
@@ -4,85 +4,135 @@ import booking.Booking;
 import user.User;
 import car.Car;
 
+/**
+ *      CLIFormatUtility is a utility class dedicated
+ *      to formatting and displaying all output within the
+ *      CarBookingCLI class.
+ */
+
 public class CLIFormatUtility {
 
-    private static final int CLI_WIDTH = 230;
+    private static final int CLI_WIDTH = 240;
     private static final int TAB_COUNT = 18;
 
-    public static final String TITLE_REGISTERED_USERS = "ALL REGISTERED USERS";
-    public static final String TITLE_ALL_CARS = "ALL AVAILABLE CARS";
+    public static final String TITLE_ALL_CARS = "AVAILABLE CARS";
     public static final String TITLE_GAS_CARS = "AVAILABLE GAS CARS";
     public static final String TITLE_ELECTRIC_CARS = "AVAILABLE ELECTRIC CARS";
-    public static final String TITLE_ALL_BOOKINGS = "ALL ACTIVE BOOKINGS";
-    public static final String TITLE_MAKE_BOOKING = "BOOKING BY USER ID AND REGISTRATION NUMBER";
-    public static final String TITLE_USER_BOOKED_CARS = "CARS BOOKED BY SPECIFIC USER";
 
-    public static final String TITLE_MAIN_MENU = "MAIN MENU";
-    public static final String TITLE_INPUT_MENU = "INPUT MENU";
+    public static final String TITLE_MAKE_BOOKING = "MAKE A CAR BOOKING BY USER ID AND REGISTRATION NUMBER";
+    public static final String TITLE_USER_BOOKED_CARS = "CARS BOOKED BY USER ID";
+
+    public static final String TITLE_ALL_BOOKINGS = "ACTIVE BOOKINGS";
+    public static final String TITLE_REGISTERED_USERS = "REGISTERED USERS";
 
     public static final String TITLE_GREETING_MENU = "🚗 WELCOME TO THE CAR BOOKING MANAGEMENT SYSTEM 🚗";
+    public static final String TITLE_MAIN_MENU = "MAIN MENU";
+    public static final String SYSTEM_GUIDELINES_MENU = "GUIDELINES";
+    public static final String USER_INPUT_MENU = "INPUT MENU";
 
+
+    public static void displayCarBookingGreeting(){
+        System.out.println(displayMenuBorder());
+        displayCenteredMenuTitles(TITLE_GREETING_MENU);
+        System.out.println(displayMenuBorder());
+    }
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
 
     public static void displayMainMenu() {
-        displayMenuSeparation();
-        displayCenteredMenu(TITLE_MAIN_MENU);
-        displayIndentedMessage("➡️","1 To Display All Available Cars");
-        displayIndentedMessage("➡️", "2 To Display Available Gasoline Cars");
-        displayIndentedMessage("➡️", "3 To Display Available Electric Cars");
-        displayIndentedMessage("➡️","4 To Make a Car Booking (By User ID and Car Registration Number)");
-        displayIndentedMessage("➡️", "5 To Display Cars Booked by a Specific User");
-        displayIndentedMessage("➡️","6 To Display All Active Bookings");
-        displayIndentedMessage("➡️", "7 To Display All Registered Users");
-        displayIndentedMessage("➡️", "8 To Exit Application");
-        displayMenuSeparation();
+        displayCenteredMenuTitles(TITLE_MAIN_MENU);
+        displayFormattedMessage("➡️","1 To Display All Available Cars");
+        displayFormattedMessage("➡️", "2 To Display Available Gasoline Cars");
+        displayFormattedMessage("➡️", "3 To Display Available Electric Cars");
+        displayFormattedMessage("➡️","4 To Make a Car Booking (By User ID and Car Registration Number)");
+        displayFormattedMessage("➡️", "5 To Display Cars Booked (By User ID)");
+        displayFormattedMessage("➡️","6 To Display All Active Bookings");
+        displayFormattedMessage("➡️", "7 To Display All Registered Users");
+        displayFormattedMessage("➡️", "8 To View Main Menu");
+        displayFormattedMessage("➡️", "9 To Exit Application");
     }
 
-    public static void displayResultsMenu(String menuName){
-        displayMenuSeparation();
-        displayCenteredMenu(menuName);
+    public static void displayResultsByMenuTitle(String menuName){
+        System.out.println();
+        System.out.println(displayMenuBorder());
+        displayCenteredMenuTitles(menuName);
+        System.out.println(displayMenuBorder());
+        System.out.println();
     }
 
-    public static void displayUserInputMenu() {
-        displayCenteredMenu(TITLE_INPUT_MENU);
-        displayIndentedMessage("➡️","Enter the number for a menu option (1-8) and press ENTER");
-        displayIndentedUserInput("➡️","Please enter your choice (1-8): ");
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+
+    public static void displayUserInput() {
+        displayCenteredMenuTitles(USER_INPUT_MENU);
+        displayFormattedUserInput("➡️","Please enter your choice (To view MAIN MENU options press 8): ");
     }
 
-    public static void displayGreeting(){
-        displayCenteredMenu(TITLE_GREETING_MENU);
+    public static void displaySystemGuidelines() {
+        displayCenteredMenuTitles(SYSTEM_GUIDELINES_MENU);
+        displayFormattedMessage("➡️","Enter the number for a menu option (1-9) and press ENTER");
+        displayFormattedMessage("➡️","Follow the prompts for specific inputs (i.e. creating a Car Booking).");
     }
 
-    public static void displayInteractionMenu() {
-        displayIndentedMessage("➡️","Enter the number for a menu option (1-8) and press ENTER");
-        displayIndentedMessage("➡️","Follow the prompts for specific inputs (i.e. creating a Car Booking).");
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    public static void displayCarsBookedByUserIdGuidelines() {
+        displayFormattedMessage("✅️","Please select a USER ID (e.g. 'b10d126a-3608-4980-9f9c-aa179f5cebc3') from the REGISTERED USERS to DISPLAY CARS BOOKED BY USER ID");
     }
 
-    public static void displayUserIDInteractionMenu() {
-        displayIndentedMessage("⚠️","Please select a USER ID from all the registered users menu (e.g., '8ca51d2b-aaaf-4bf2-834a-e02964e10fc3')");
+    public static void displayUserIdBookingGuidelines() {
+        displayFormattedMessage("✅️","Please select a USER ID (e.g. 'b10d126a-3608-4980-9f9c-aa179f5cebc3') from the REGISTERED USERS to MAKE A BOOKING");
     }
 
-    public static void displayRegistrationNumberInteractionMenu() {
-        displayIndentedMessage("⚠️","Please select a REGISTRATION NUMBER from the available cars menu (e.g., '123_3')");
+    public static void displayRegistrationNumberBookingGuidelines() {
+        displayFormattedMessage("✅","Please select a REGISTRATION NUMBER from the AVAILABLE CARS MENU (e.g., '123_3')");
     }
 
-    public static void displayBookingDetailsFormatted(Booking booking){
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    public static void displayFormattedBookingDetails(Booking booking){
 
         StringBuilder bookingDetailsFormatted = new StringBuilder();
-        String indentation = getIndentedMenuItem();
+        String indentation = alignOutputByTabCount();
 
         bookingDetailsFormatted.append(indentation).append("Booking ID:             ").append(booking.getUserBookingID()).append("\n");
         bookingDetailsFormatted.append(indentation).append("Booking time:           ").append(booking.getBookingTime()).append("\n");
         bookingDetailsFormatted.append(indentation).append("Booking Cancelled:      ").append(booking.isBookingCancelled()).append("\n");
         System.out.println(bookingDetailsFormatted);
 
-        displayUserDetailsFormatted(booking.getUser());
-        displayCarDetailsFormatted(booking.getCar());
+        displayFormattedUserDetails(booking.getUser());
+        displayFormattedCarDetails(booking.getCar());
     }
 
-    public static void displayUserDetailsFormatted(User user){
+    public static void displayFormattedUserDetails(User user){
 
         StringBuilder userDetailsFormatted = new StringBuilder();
-        String indentation = getIndentedMenuItem();
+        String indentation = alignOutputByTabCount();
 
         userDetailsFormatted.append(indentation).append("Name:        ").append(user.getName()).append("\n");
         userDetailsFormatted.append(indentation).append("Last Name:   ").append(user.getLastName()).append("\n");
@@ -91,10 +141,10 @@ public class CLIFormatUtility {
         System.out.println(userDetailsFormatted);
     }
 
-    public static void displayCarDetailsFormatted(Car Car){
+    public static void displayFormattedCarDetails(Car Car){
 
         StringBuilder carDetailsFormatted = new StringBuilder();
-        String indentation = getIndentedMenuItem();
+        String indentation = alignOutputByTabCount();
 
         carDetailsFormatted.append(indentation).append("Manufacturer:            ").append(Car.getManufacturer()).append("\n");
         carDetailsFormatted.append(indentation).append("Fuel Type:               ").append(Car.getFuelType().toString()).append("\n");
@@ -104,42 +154,78 @@ public class CLIFormatUtility {
         System.out.println(carDetailsFormatted);
     }
 
-    private static void displayCenteredMenu(String title){
-        System.out.println(getCenteredMenuTitle(title));
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    private static void displayCenteredMenuTitles(String menuTitles){
+        System.out.println(centerMenuTitles(menuTitles));
     }
 
-    private static String getCenteredMenuTitle(String title) {
-        int titlePadding = (getMenuSeparation().length() - title.length()) / 2;
-        String centeredTitle = String.format("%" + titlePadding + "s%s", "", title);
+    private static String centerMenuTitles(String menuTitle) {
+
+        // Total amount of empty space available on both the left and right sides of the title
+        int titlePadding = (displayMenuBorder().length() - menuTitle.length()) / 2;
+
+        // [titlePadding spaces] + [title string]
+        String centeredTitle = String.format("%" + titlePadding + "s%s", "", menuTitle);
+
+        // centeredTitle appears on its own line
         return "\n" + centeredTitle + "\n";
     }
 
-    private static void displayMenuSeparation(){
-        System.out.println("\n" + getMenuSeparation());
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    public static void displayFormattedMessage(String messageIcon, String message) {
+        System.out.println(getFormattedMessage(messageIcon, message));
     }
 
-    private static String getMenuSeparation(){
+    private static String getFormattedMessage(String messageIcon, String message) {
+        return alignOutputByTabCount() + messageIcon + " " + message;
+    }
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    public static void displayFormattedUserInput(String symbol, String message) {
+        System.out.print(getFormattedUserInput(symbol, message));
+    }
+
+    private static String getFormattedUserInput(String messageIcon, String message) {
+        return alignOutputByTabCount() + messageIcon + " " + message;
+    }
+
+    /**
+     *       TODO add documentation
+     *
+     *
+     *
+     *
+     * */
+
+    private static String displayMenuBorder(){
         return "=".repeat(CLI_WIDTH);
     }
 
-    private static String getIndentedMenuItem() {
+    private static String alignOutputByTabCount() {
         return "\t".repeat(TAB_COUNT);
-    }
-
-    private static String getIndentedMessage(String symbol, String message) {
-        return getIndentedMenuItem() + symbol + " " + message;
-    }
-
-    public static void displayIndentedMessage(String symbol, String message) {
-        System.out.println(getIndentedMessage(symbol, message));
-    }
-
-    private static String getIndentedUserInput(String symbol, String message) {
-        return getIndentedMenuItem() + symbol + " " + message;
-    }
-
-    public static void displayIndentedUserInput(String symbol, String message) {
-        System.out.print(getIndentedUserInput(symbol, message));
     }
 
 }
