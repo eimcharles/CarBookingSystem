1e679d4882b87126a2f9a8d6e69bea8c0f34caf4
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-27 16:02:55 -0500

    Docs: Add Javadoc comments to all methods in CarUnavailableException

--- src/exception/CarUnavailableException.java
+++ src/exception/CarUnavailableException.java
@@ -1,14 +1,20 @@
 package exception;
 
 /**
- *      CarUnavailableException is a
- *      custom exception thrown when a
+ *      CarUnavailableException class throws
+ *      a custom exception when a
  *      request is made for a Car that
- *      is currently booked by another user
- *
+ *      is currently booked by another user.
  * */
 
 public class CarUnavailableException extends RuntimeException {
+
+    /**
+     *   Constructs a new CarUnavailableException
+     *   with a detailed message indicating
+     *   the registration number for the car
+     *   that is currently booked by another user
+     * */
 
     public CarUnavailableException(String registrationNumber) {
         super(String.format("Car with registration number '%s' is currently booked and unavailable.", registrationNumber));
