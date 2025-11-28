4c6e8a0d25f6f95e425f09c265b10183a644bca6
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-28 10:38:01 -0500

    Refactor(CarDAO): converting to instance-based repository.
    The CarDAO class has been refactored from a static, class-level implementation to an instance-based (non-static) DAO to enable Dependency Injection (DI).

--- src/car/CarDAO.java
+++ src/car/CarDAO.java
@@ -10,24 +10,23 @@ import java.util.Arrays;
 
 public class CarDAO {
 
-    private static final Car[] carDAO;
+    private final Car[] carDAO;
 
-    static {
-
-        carDAO = new Car[]{
+    public CarDAO() {
+        this.carDAO = new Car[]{
 
                 new Car("123_1", new BigDecimal("89.00"), Brand.BMW, FuelType.ELECTRIC),
                 new Car("123_2", new BigDecimal("79.00"), Brand.BMW, FuelType.GASOLINE),
                 new Car("123_3", new BigDecimal("69.00"), Brand.VOLKSWAGEN, FuelType.ELECTRIC),
                 new Car("123_4", new BigDecimal("49.00"), Brand.HONDA, FuelType.ELECTRIC),
-                new Car("123_5", new BigDecimal("59.00"), Brand.HONDA,  FuelType.GASOLINE),
+                new Car("123_5", new BigDecimal("59.00"), Brand.HONDA, FuelType.GASOLINE),
 
         };
     }
 
-    public static Car[] getCarsFromDAO() {
+    public Car[] getCarsFromDAO() {
         // Copy of Car objects returned from carDAO
-        return Arrays.copyOf(carDAO, carDAO.length);
+        return Arrays.copyOf(this.carDAO, this.carDAO.length);
     }
 
 }
