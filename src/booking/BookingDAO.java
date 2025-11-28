1002f22552afd512a1a1149e65971a81347793ef
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-11-28 10:36:20 -0500

    Refactor(BookingDAO): converting to instance-based repository.
    The BookingDAO class has been refactored from a static, class-level implementation to an instance-based (non-static) DAO to enable Dependency Injection (DI).

--- src/booking/BookingDAO.java
+++ src/booking/BookingDAO.java
@@ -5,45 +5,43 @@ import java.util.Arrays;
 /**
  *      Data Access Object (DAO) class for managing Car Booking objects.
  *      This implementation stores and manages Car Bookings using arrays.
- *
- *      TODO: Implement remaining Booking DAO functionality
  */
 
 public class BookingDAO {
 
-    private static final Booking[] bookingsDao;
+    private final Booking[] bookingsDao;
 
     // Tracks the index of the next available slot in bookingsDao
-    private static int nextAvailableIndex = 0;
+    private int nextAvailableIndex = 0;
 
     // Defines the fixed, maximum size of the array storage.
     private static final int MAX_CAPACITY = 10;
 
-    static {
+    public BookingDAO() {
 
         // Storage for available bookings
-        bookingsDao = new Booking[MAX_CAPACITY];
+        this.bookingsDao = new Booking[MAX_CAPACITY];
 
     }
 
-    public static void addBooking(Booking carBooking) {
+    public  void addBooking(Booking carBooking) {
 
         // Checks if index of next available slot has reached length of array
-        if (nextAvailableIndex >= bookingsDao.length) {
-            throw new IllegalStateException(String.format("No more bookings available, total slots available %d", bookingsDao.length));
+        if (this.nextAvailableIndex >= this.bookingsDao.length) {
+            throw new IllegalStateException(String.format("No more bookings available, total slots available %d", this.bookingsDao.length));
         }
 
         // Adds the carBooking into the slot that nextAvailableIndex points to
-        bookingsDao[nextAvailableIndex] = carBooking;
+        this.bookingsDao[this.nextAvailableIndex] = carBooking;
 
         // Moves the index to point to the following slot
-        nextAvailableIndex++;
+        this.nextAvailableIndex++;
 
     }
 
-    public static Booking[] getBookingDao() {
+    public Booking[] getBookingDao() {
         // Copy of bookingsDao returned from BookingDAO
-        return Arrays.copyOf(bookingsDao, bookingsDao.length);
+        return Arrays.copyOf(this.bookingsDao, this.bookingsDao.length);
     }
 
 }
