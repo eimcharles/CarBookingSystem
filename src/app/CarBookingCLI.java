20850bf950e085f20b566f5c6d26f831f6406cdf
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-04 15:03:20 -0500

    Refactor (Single responsibility):
    Introducing CarBookingCLI, central controller that takes user input, calls the business services, and orchestrates the display of results.

--- src/app/CarBookingCLI.java
+++ src/app/CarBookingCLI.java
@@ -1,23 +1,30 @@
 package app;
 
-import booking.Booking;
 import booking.BookingService;
+
 import car.Car;
-import exception.*;
 import user.User;
 import user.UserService;
+
+import exception.CarNotFoundException;
+import exception.CarUnavailableException;
 
 import java.util.Scanner;
 import java.util.UUID;
 
 import static app.CLIFormatUtility.*;
-import static app.CLIFormatUtility.displayFormattedCarDetails;
+import static app.CLIInputUtility.*;
+import static app.CLIDisplayUtility.*;
 
 /**
- *      TODO write comment for CarBookingCLI Class
+ *      CarBookingCLI class is the central controller
+ *      that takes user input,
+ *      calls the business services,
+ *      and orchestrates the display of results.
+ *
  *      TODO write comment for all methods CarBookingCLI
  *      TODO finish implementation for CarBookingCLI Class
- *      TODO Refactor class for single responsibility principle
+ *
  * */
 
 public class CarBookingCLI {
@@ -28,10 +35,10 @@ public class CarBookingCLI {
      *      1.Displays necessary menus and user instructions defined by `TITLE_MAKE_BOOKING`.
      *
      *      2. Guides the user to select, and validates a User ID for new car booking
-     *      by using 'promptAndValidateUserID()' helper method.
+     *      by using 'promptAndValidateUserID()' CLIDisplayUtility method.
      *
      *      2. Guides the user to select, and validates a car registration number for new car booking
-     *      by using 'promptAndValidateCarRegistrationNumber()' helper method.
+     *      by using 'promptAndValidateCarRegistrationNumber()' CLIDisplayUtility method.
      *
      *      4. Once both identifiers are successfully validated, it delegates
      *      to the `processBookingTransaction()` helper method to make
@@ -113,65 +120,6 @@ public class CarBookingCLI {
 
     }
 
-    private static String promptAndValidateCarRegistrationNumber(BookingService bookingService, Scanner scanner){
-
-        // User input "123_1"
-        String userRegistrationInput;
-
-        // Flag for input validation
-        boolean isValidInput = false;
-
-        do {
-
-            displayFormattedUserInput("➡️", "Please enter the Car Registration Number: ");
-            userRegistrationInput = scanner.nextLine().trim();
-
-            // Handles empty input
-            if (userRegistrationInput.isEmpty()) {
-                displayFormattedMessage("⚠️", "Car Registration Number cannot be empty - Please try again.");
-
-                // Empty input: Skip the rest of the loop
-                continue;
-
-            }
-
-            try {
-
-                // Validates available car: existence and car current availability
-                bookingService.getUnbookedCarForBooking(userRegistrationInput);
-
-                // Car exists and is currently available
-                isValidInput = true;
-
-            } catch (CarNotFoundException e) {
-
-                // Car does not exist
-                displayFormattedMessage("⚠️", e.getMessage());
-
-            } catch (CarUnavailableException e) {
-
-                // Car is unavailable
-                displayFormattedMessage("⚠️", e.getMessage());
-
-            } catch (Exception e) {
-
-                // Catches all unforeseen system errors
-                displayFormattedMessage("❌", "Booking attempt failed - Please try again.");
-
-            }
-
-            // Re-display on invalid inputs
-            if (!isValidInput){
-                displayAllAvailableCars(bookingService);
-                displayRegistrationNumberBookingGuidelines();
-            }
-
-        } while (!isValidInput);
-
-        return userRegistrationInput;
-
-    }
-
     /**
      *      TODO add JavaDocs
      *
@@ -185,147 +133,12 @@ public class CarBookingCLI {
     }
 
     /**
-     *      Retrieves all currently available
-     *      cars from the BookingService and
-     *      prints them to the console using the
-     *      standardized `displayFormattedCarDetails()` method.
-     *
-     *      Availability means the car is not tied to any active booking.
-     *
-     *      If no cars are available, a corresponding message
-     *      is displayed using `displayFormattedMessage()` method.
-     *
-     *      The results are presented under the menu title
-     *      defined by `TITLE_ALL_CARS`.
-     *
-     *      @param bookingService
-     *
-     *      The BookingService instance used to fetch the availability data.
-     */
-
-    public static void displayAllAvailableCars(BookingService bookingService) {
-
-        Car[] availableCars = bookingService.getAllUnbookedCars();
-
-        displayResultsByMenuTitle(TITLE_ALL_CARS);
-        if (availableCars == null || availableCars.length == 0) {
-            displayFormattedMessage("❌", "No cars currently available for booking");
-            return;
-        }
-
-        for (Car availableCar : availableCars) {
-            displayFormattedCarDetails(availableCar);
-        }
-    }
-
-    /**
-     *      Retrieves all currently available gas cars from the
-     *      BookingService and prints them to the console using the
-     *      standardized `displayFormattedCarDetails()` method.
-     *
-     *      Availability means the gas car is not tied to any active booking.
-     *
-     *      If no gas cars are available, a corresponding message
-     *      is displayed using `displayFormattedMessage()` method.
-     *
-     *      The results are presented under the menu title
-     *      defined by `TITLE_GAS_CARS`.
-     *
-     *      @param bookingService
-     *
-     *      The BookingService instance used to fetch the filtered availability data.
-     */
-
-    public static void displayAllAvailableGasCars(BookingService bookingService) {
-
-        Car[] availableGasCars = bookingService.getAllUnbookedGasCars();
-
-        displayResultsByMenuTitle(TITLE_GAS_CARS);
-        if (availableGasCars == null || availableGasCars.length == 0) {
-            displayFormattedMessage("❌", "No gas cars currently available for booking");
-            return;
-        }
-
-        for (Car availableGasCar : availableGasCars) {
-            displayFormattedCarDetails(availableGasCar);
-        }
-    }
-
-    /**
-     *      Retrieves all currently available
-     *      electric cars from the BookingService
-     *      and prints them to the console using the
-     *      standardized `displayFormattedCarDetails()` method.
-     *
-     *      Availability means the electric car is not tied to any active booking.
-     *
-     *      If no electric cars are available, a corresponding message
-     *      is displayed using `displayFormattedMessage()` method.
-     *
-     *      The results are presented under the menu title
-     *      defined by `TITLE_ELECTRIC_CARS`.
-     *
-     *      @param bookingService
-     *
-     *      The BookingService instance used to fetch the filtered availability data.
-     */
-
-    public static void displayAllAvailableElectricCars(BookingService bookingService) {
-
-        Car[] availableElectricCars = bookingService.getAllUnbookedElectricCars();
-
-        displayResultsByMenuTitle(TITLE_ELECTRIC_CARS);
-        if (availableElectricCars == null || availableElectricCars.length == 0) {
-            displayFormattedMessage("❌", "No electric cars currently available for booking");
-            return;
-        }
-
-        for (Car availableEelectricCar : availableElectricCars) {
-            displayFormattedCarDetails(availableEelectricCar);
-        }
-    }
-
-    /**
-     *      Retrieves all active
-     *      bookings from the BookingService
-     *      and prints them to the console using the
-     *      standardized `displayFormattedBookingDetails()` method.
-     *
-     *      If no active bookings are found,
-     *      a corresponding message is displayed
-     *      using `displayFormattedMessage()` method.
-     *
-     *      The results are presented under the menu title
-     *      defined by `TITLE_ALL_BOOKINGS`.
-     *
-     *      @param bookingService
-     *
-     *      The BookingService instance used to fetch the data.
-     *
-     */
-
-    public static void displayAllActiveBookings(BookingService bookingService){
-
-        Booking[] allBookings = bookingService.getBookings();
-
-        displayResultsByMenuTitle(TITLE_ACTIVE_BOOKINGS);
-        if (allBookings == null || allBookings.length == 0){
-            displayFormattedMessage("❌", "No bookings currently registered in the system");
-        }
-
-
-        for (Booking allBooking : allBookings) {
-            displayFormattedBookingDetails(allBooking);
-        }
-    }
-
-    /**
      *      Retrieves all cars that are associated to a user from
      *      the BookingService and prints them to the console using the
      *      standardized `formatAndDisplayUserBookedCars()` method.
      *
      *      This method guides the user to select, and validates a User ID
-     *      using 'promptAndValidateUserID()' helper method.
+     *      using 'promptAndValidateUserID()' CLIDisplayUtility method.
      *
      *      Once the user input is validated it then
      *      retrieves the corresponding cars currently booked by that user.
@@ -367,116 +180,4 @@ public class CarBookingCLI {
         formatAndDisplayUserBookedCars(bookedCars, validatedUserId.toString());
     }
 
-    private static void formatAndDisplayUserBookedCars(Car[] bookedCars, String validatedUserId){
-
-        if (bookedCars == null || bookedCars.length == 0){
-
-            // No bookings for the given user id
-            displayResultsByMenuTitle(TITLE_USER_BOOKED_CARS);
-            displayFormattedMessage("❌", "User ID '" + validatedUserId + "' has no active car bookings.");
-
-        } else {
-
-            // Display and format user booked car results
-            displayResultsByMenuTitle(TITLE_USER_BOOKED_CARS);
-            for (Car bookedCar : bookedCars) {
-                displayFormattedCarDetails(bookedCar);
-            }
-        }
-    }
-
-    private static UUID promptAndValidateUserID(UserService userService, Scanner scanner){
-
-        // User Input '8ca51d2b-aaaf-4bf2-834a-e02964e10fc3'
-        String userIdInput;
-
-        // Holds the validated and converted userId object
-        UUID userId = null;
-
-        // Flag for input validation
-        boolean isValidInput = false;
-
-        do {
-
-            displayFormattedUserInput("➡️", "Please enter the User ID: ");
-            userIdInput = scanner.nextLine().trim();
-
-            // Handles empty input
-            if (userIdInput.isEmpty()) {
-                displayFormattedMessage("⚠️", "User ID cannot be empty - Please try again.");
-
-                // Empty input: Skip the rest of the loop
-                continue;
-            }
-
-            try {
-
-                // Validates Input: Convert the input string to a UUID object.
-                userId = UUID.fromString(userIdInput);
-
-                // Validates User Existence: throws if user doesn't exist.
-                userService.getUserById(userId);
-
-                // Input and user exist and are valid.
-                isValidInput = true;
-
-            } catch (IllegalArgumentException e) {
-
-                // Handles bad UUID format
-                displayFormattedMessage("❌","Invalid User ID Format: "  + userIdInput);
-
-            } catch (UserNotFoundException e) {
-
-                // Handles User non-existence
-                displayFormattedMessage("❌",e.getMessage());
-
-            } catch (Exception e) {
-
-                // Catches all unforeseen system errors
-                displayFormattedMessage("❌", "Displaying users booked cars failed - Please try again.");
-
-            }
-
-            // Re-display on invalid inputs
-            if (!isValidInput){
-                displayAllRegisteredUsers(userService);
-                displayUserIdBookingGuidelines();
-            }
-
-        } while (!isValidInput);
-
-        return userId;
-
-    }
-
-    /**
-     *        Retrieves all registered users from the
-     *        UserService and prints them to the console
-     *        using the standardized `displayFormattedUserDetails()` method.
-     *
-     *        If no users are found, a corresponding message
-     *        is displayed using `displayFormattedMessage()` method.
-     *
-     *        The results are presented under the menu title
-     *        defined by `TITLE_REGISTERED_USERS`.
-     *
-     *        @param userService
-     *
-     *        The UserService instance used to fetch the user data.
-     */
-
-    public static void displayAllRegisteredUsers(UserService userService){
-
-        User[] users = userService.getUsers();
-
-        displayResultsByMenuTitle(TITLE_REGISTERED_USERS);
-        if (users == null || users.length == 0){
-            displayFormattedMessage("❌", "No users currently registered in the system");
-            return;
-        }
-
-        for (User user : users) {
-            displayFormattedUserDetails(user);
-        }
-    }
 }
