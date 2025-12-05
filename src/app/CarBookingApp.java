e374341ef19a4d41f55638612c9171d8739d2fb8
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-04 21:35:43 -0500

    Moving the displayResultsByMenuTitle into the runCarBookingApp() method.

--- src/app/CarBookingApp.java
+++ src/app/CarBookingApp.java
@@ -47,34 +47,42 @@ public class CarBookingApp {
                 switch (userInput) {
                     case "1":
                         // To Make a Car Booking (By User ID and Car Registration Number)
+                        displayResultsByMenuTitle(TITLE_MAKE_BOOKING);
                         makeACarBookingByUserIdAndRegistrationNumber(configuration.getUserService(), configuration.getBookingService(), scanner);
                         break;
                     case "2":
                         // To Cancel a Car Booking (By Booking ID)
+                        displayResultsByMenuTitle(TITLE_CANCEL_BOOKING);
                         cancelCarBookingByBookingId(configuration.getBookingService(), scanner);
                         break;
                     case "3":
                         // To Display All Available Cars
+                        displayResultsByMenuTitle(TITLE_ALL_CARS);
                         displayAllAvailableCars(configuration.getBookingService());
                         break;
                     case "4":
                         // To Display Available Gasoline Cars
+                        displayResultsByMenuTitle(TITLE_GAS_CARS);
                         displayAllAvailableGasCars(configuration.getBookingService());
                         break;
                     case "5":
                         // To Display Available Electric Cars
+                        displayResultsByMenuTitle(TITLE_ELECTRIC_CARS);
                         displayAllAvailableElectricCars(configuration.getBookingService());
                         break;
                     case "6":
                         // To Display All Active Bookings
+                        displayResultsByMenuTitle(TITLE_ACTIVE_BOOKINGS);
                         displayAllActiveBookings(configuration.getBookingService());
                         break;
                     case "7":
                         // To Display Cars Booked by User (By User ID)
+                        displayResultsByMenuTitle(TITLE_USER_BOOKED_CARS);
                         displayUserBookedCarsByUserId(configuration.getUserService(), configuration.getBookingService(), scanner);
                         break;
                     case "8":
                         // To Display All Registered Users
+                        displayResultsByMenuTitle(TITLE_REGISTERED_USERS);
                         displayAllRegisteredUsers(configuration.getUserService());
                         break;
                     case "9":
