8b0c74c87826e35177212adc909d0849cc857356
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2025-12-08 17:10:29 -0500

    Docs: Adding Javadocs for methods in BookingNotActiveException class.

--- src/exception/BookingNotActiveException.java
+++ src/exception/BookingNotActiveException.java
@@ -6,10 +6,17 @@ import java.util.UUID;
  *      BookingUnavailableException class throws
  *      a custom exception when a
  *      request is made for a Booking that
- *      is currently not active
+ *      is currently not active.
  * */
 
 public class BookingNotActiveException extends RuntimeException {
+
+    /**
+     *   Constructs a new BookingNotActiveException
+     *   with a detailed message indicating the
+     *   booking currently not active.
+     * */
+
     public BookingNotActiveException(UUID bookingId) {
         super(String.format("Booking with booking id '%s' is currently not active (already cancelled).", bookingId));
     }
