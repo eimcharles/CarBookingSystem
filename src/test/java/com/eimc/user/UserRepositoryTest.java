b3e03fbedd0c323798b00d636451d5132e7e9e8d
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-01-29 15:20:18 -0500

    Spring Boot refactor: removing test cases for user domain, service, and repository

--- src/test/java/com/eimc/user/UserRepositoryTest.java
+++ src/test/java/com/eimc/user/UserRepositoryTest.java
@@ -1,16 +1,7 @@
 package com.eimc.user;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import java.util.UUID;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 /**
- *      Unit tests for the ListUserDAO class
+ *      Unit tests for the User Repository class
  *
  *      Test methods follow the Arrange-Act-Assert (AAA) pattern,
  *      commonly labeled as Given-When-Then:
@@ -22,75 +13,5 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserRepositoryTest {
 
-    private UserRepository actualTestUserRepository;
-
-    private User expectedTestUserCharles;
-    private User expectedTestUserJerry;
-
-    @BeforeEach
-    void setUp(){
-
-        // GIVEN
-        actualTestUserRepository = new UserRepository();
-
-        expectedTestUserJerry = new User(
-                UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"),
-                "Jerry",
-                "LeBlond");
-
-    }
-
-    @Test
-    void getUsersCanReturnADefensiveCopyAndExternalModificationDoesNotAffectInternalState(){
-
-        // GIVEN actualTestListUserDAO object created in setUp();
-
-        // WHEN actualTestUsers
-        List<User> actualTestUsers = actualTestUserRepository.getUsers();
-        actualTestUsers.set(0, null);
-        List<User> actualTestUsersAfterModification = actualTestUserRepository.getUsers();
-
-        // THEN
-        assertThat(actualTestUsersAfterModification.get(0))
-                .as("The element at index 0 in actualTestUsers state should not be null")
-                .isNotNull();
-
-    }
-
-    @Test
-    void getUserByIdCanReturnCorrespondingUserById(){
-
-        // GIVEN
-        UUID testTargetId = expectedTestUserJerry.getUserId();
-
-        // WHEN
-        User actualUserReturnedById = actualTestUserRepository.getUserById(testTargetId);
-
-        // THEN
-        assertThat(actualUserReturnedById).as("The getUserById() method must return a user with the correct user id.")
-                .isNotNull()
-                .isEqualTo(expectedTestUserJerry);
-
-    }
-
-    @Test
-    void getUserByIdCanThrowUserNotFoundExceptionWhenUserIdDoesntExist(){
-
-        // GIVEN
-        UUID nonExistentId = UUID.randomUUID();
-
-        /**
-         *     Functional Programming:
-         *
-         *     Asserts that calling getUserById() with a non-existent ID (within the lambda)
-         *     throws a UserNotFoundException, and verifies the exception message contains the missing ID.
-         * */
-
-        // WHEN & THEN
-        assertThatThrownBy(() -> actualTestUserRepository.getUserById(nonExistentId))
-                .isInstanceOf(UserNotFoundException.class)
-                .hasMessageContaining(nonExistentId.toString());
-
-    }
 
 }
