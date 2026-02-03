588f0f9e90805179e079364b0d547c702901fc81
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-02-03 11:55:03 -0500

    feat(user): implement UserResponseDTO for secure data transfer.

---
+++ src/main/java/com/eimc/user/UserResponseDTO.java
@@ -0,0 +1,37 @@
+package com.eimc.user;
+
+import com.eimc.user.model.User;
+import com.eimc.user.model.UserRole;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record UserResponseDTO (
+
+        UUID userId,
+        String firstName,
+        String lastName,
+        String email,
+        String phoneNumber,
+        UserRole userRole,
+
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
+        LocalDateTime createdAt
+
+) {
+    public static UserResponseDTO from(User user) {
+        return new UserResponseDTO(
+
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPhoneNumber(),
+                user.getUserRole(),
+                user.getCreatedAt()
+
+        );
+    }
+
+}
