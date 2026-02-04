6107b4dc134cd4e9d5ef90e58978eb9bdc28777a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-02-04 15:39:22 -0500

    change(user): annotate UserRepository

--- src/main/java/com/eimc/user/repository/UserRepository.java
+++ src/main/java/com/eimc/user/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.eimc.user.repository;
 
 import com.eimc.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUserId(UUID userId);
