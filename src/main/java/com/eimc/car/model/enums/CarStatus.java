c73219ce88e1981013ef6de36ebd017862efedee
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-02-06 14:25:03 -0500

    feat(car): add RESERVED status and unify booking logic.

--- src/main/java/com/eimc/car/model/enums/CarStatus.java
+++ src/main/java/com/eimc/car/model/enums/CarStatus.java
@@ -2,7 +2,13 @@ package com.eimc.car.model.enums;
 
 public enum CarStatus {
 
+    /// Available for booking
     AVAILABLE(false),
+
+    /// Reserved online and ready for pickup
+    RESERVED(true),
+
+    /// Booked and picked up
     BOOKED(true);
 
     private final boolean isBooked;
